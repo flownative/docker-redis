230d0b2fcc5acc46a77ed607a9c4a1aad68d5bf7
Robert Lemke <robert@flownative.com>
2020-08-26 17:41:06 +0200

    Run Redis as user "redis"
    Previously, a user 1000 without a proper gid was used.

--- root-files/build.sh
+++ root-files/build.sh
@@ -4,6 +4,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Modify the existing user and group "redis":
+
+groupmod --gid 1000 redis
+usermod --uid 1000 --gid 1000 --shell /usr/sbin/nologin --home "${REDIS_BASE_PATH}" redis
+
 mkdir -p \
     "${REDIS_BASE_PATH}/etc" \
     "${REDIS_BASE_PATH}/bin" \
@@ -14,7 +19,11 @@ mv /usr/bin/redis* "${REDIS_BASE_PATH}/bin/"
 
 chown -R root:root "${REDIS_BASE_PATH}"
 chmod -R g+rwX "${REDIS_BASE_PATH}"
-chmod 664 "${REDIS_BASE_PATH}/etc/redis-default.conf"
 
 chown -R root:root /var/lib/redis
-chmod -R g+rwX /var/lib/redis
+chmod -R 755 /var/lib/redis
+
+chmod 664 "${REDIS_BASE_PATH}/etc/redis-default.conf"
+chown redis:redis "${REDIS_BASE_PATH}/etc/redis-default.conf"
+chmod 775 "${REDIS_BASE_PATH}/etc"
+chown redis:redis "${REDIS_BASE_PATH}/etc"
